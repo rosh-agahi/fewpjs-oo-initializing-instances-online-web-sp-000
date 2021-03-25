0040292c14d7040a4b9611d12952ff0efa330d35
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-25 17:17:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 // Write your code here
-class breakfast {
+class Breakfast {
+  constructor(drink, food){
+    this.drink = drink;
+    this.food = food;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
   
 }
 
-class lunch {
-  
-}
-
-class dinner {
+class Dinner {
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
   
 }
